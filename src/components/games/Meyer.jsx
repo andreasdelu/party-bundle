c3aa9d9396e1216620b0d8cd7f2982f636e49647
andreasdelu <Andreasdelu@gmail.com>
2022-10-12 15:43:23 +0200

    yessir Co-authored-by: Runelc <Runelc@users.noreply.github.com>

--- src/components/games/Meyer.jsx
+++ src/components/games/Meyer.jsx
@@ -4,8 +4,8 @@ import "../../styles/Games.css";
 export default function Meyer({ diff }) {
 	return (
 		<>
-			<h1>Meyer</h1>
-			<h2>Difficulty: {diff}</h2>
+			<h1 className='gameTitle'>Meyer</h1>
+			<small>Difficulty: {diff}</small>
 		</>
 	);
 }
